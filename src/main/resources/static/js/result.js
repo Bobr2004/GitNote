97d788bd75f7b63454225aafe18a7a03526bb812
bohdan <bohdenshoshka@gmail.com>
2023-07-12 23:07:23 +0300

    added favicon

--- src/main/resources/static/js/result.js
+++ src/main/resources/static/js/result.js
@@ -1,18 +1,23 @@
 const result = document.querySelector(".result__score")
 const imag = document.querySelector(".result__image")
+
+console.log("chlen")
+
 let col;
 imag.classList.remove("result__image-good")
 imag.classList.remove("result__image-bad")
 const shzh = {
-    "good": "#349048",
+    "good": "#349048FF",
     "bad": '#dc383c'
 }
 
 if (Number(result.innerText) >= 3) {
     imag.classList.add("result__image-good")
     result.style.color = shzh.good
-}
-else {
+} else {
     imag.classList.add("result__image-bad")
     result.style.color = shzh.bad
 }
+
+result.innerText = Number(result.innerText) * 25 + "%"
+
